05c56ec140011564ef3ccbe3d9bf8c28890e9d93
Sha19112006 <sha19112006@gmail.com>
2026-09-02 07:34:09 +0530

    parent code

---
+++ python _project.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+class Student(ABC):
+    def __init__(self, name, roll_no):
+        
+        self.__name = name
+        self.__roll_no = roll_no
+
+    def get_name(self):
+        return self.__name
+
+    def get_roll_no(self):
+        return self.__roll_no
